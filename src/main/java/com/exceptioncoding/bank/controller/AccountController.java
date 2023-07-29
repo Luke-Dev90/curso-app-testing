1c1ce9296374af789cb52009282f3e6052e21081
lucaschalela990 <lukeesf2016@gmail.com>
2023-07-29 16:58:58 -0300

    refactor and format master

--- src/main/java/com/exceptioncoding/bank/controller/AccountController.java
+++ src/main/java/com/exceptioncoding/bank/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.exceptioncoding.bank.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,19 +13,19 @@ import com.exceptioncoding.bank.dto.TransferDTO;
 import com.exceptioncoding.bank.service.AccountService;
 
 @RestController
-@RequestMapping("/home-baking")
+@RequestMapping("/home-banking")
 public class AccountController {
 
 	@Autowired
 	private AccountService service;
 	
 	@PostMapping("/transfer")
-	public ResponseEntity<ResponseTransferDTO> transfer(TransferDTO transferDTO) throws Exception{
+	public ResponseEntity<ResponseTransferDTO> transfer(@RequestBody TransferDTO transferDTO) throws Exception{
 		return this.service.transferAmount(transferDTO);
 	}
 	
 	@PostMapping("/create")
-	public ResponseEntity<String> create(AccountDTO accountDTO){
+	public ResponseEntity<String> create(@RequestBody AccountDTO accountDTO){
 		return this.service.createAccount(accountDTO);
 	}
 	
